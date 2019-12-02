e10e7faa4ca4cb9ffbfca724a0ff692ec33e2c15
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-12-02 09:25:53 +0100

    Day1: part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -4,18 +4,32 @@ fn main() {
     let input = fs::read_to_string("./input/day1.txt")
         .expect("Please put your input under ./input/day1.txt");
 
-    let input: Vec<f64> = input
+    let input: Vec<u32> = input
         .lines()
         .into_iter()
-        .map(|line| line.parse::<f64>().unwrap())
+        .map(|line| line.parse::<u32>().unwrap())
         .collect();
 
-    let result: u32 = input
+    let fuel: u32 = input
         .into_iter()
-        .map(|mass| mass / 3.)
-        .map(|mass| mass as u32)
-        .map(|mass| mass - 2)
+        .map(|mass| calculate_fuel_for_module(mass))
         .sum();
 
-    println!("{}", result);
+    println!("{}", fuel);
+}
+
+fn calculate_fuel_for_module(mass: u32) -> u32 {
+    let mut fuel_total = mass / 3 - 2;
+    let mut fuel_needed = fuel_total as i32;
+
+    loop {
+        fuel_needed = fuel_needed / 3 - 2;
+        if fuel_needed <= 0 {
+            break;
+        } else {
+            fuel_total += fuel_needed as u32;
+        }
+    }
+
+    fuel_total
 }

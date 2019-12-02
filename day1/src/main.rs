d5f87f4aa841a63596efba934442c5535511535c
Lucas Malandrino <lucas.malandrino@gmail.com>
2019-12-02 09:13:29 +0100

    Day1: part 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,21 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string("./input/day1.txt")
+        .expect("Please put your input under ./input/day1.txt");
+
+    let input: Vec<f64> = input
+        .lines()
+        .into_iter()
+        .map(|line| line.parse::<f64>().unwrap())
+        .collect();
+
+    let result: u32 = input
+        .into_iter()
+        .map(|mass| mass / 3.)
+        .map(|mass| mass as u32)
+        .map(|mass| mass - 2)
+        .sum();
+
+    println!("{}", result);
 }
